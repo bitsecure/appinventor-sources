0fb6a5cb9484a6f8f2f6a7f1286d008b1dcef439
Mark D. Adams <deadluser@gmail.com>
2013-08-30 08:07:46 -0400

    Add GPL 3 license to Alarm component.

--- appinventor/components/src/com/google/appinventor/components/runtime/Alarm.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/Alarm.java
@@ -1,7 +1,20 @@
 // -*- mode: java; c-basic-offset: 2; -*-
-// Copyright 2009-2011 Google, All Rights reserved
-// Copyright 2011-2012 MIT, All rights reserved
-// Released under the MIT License https://raw.github.com/mit-cml/app-inventor/master/mitlicense.txt
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.google.appinventor.components.runtime;
 
 import android.app.Activity;
